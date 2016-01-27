91a72311b0009b1f139e685ff0b75c3c48896ffd
websideas <websideas.corp@gmail.com>
2016-01-27 14:17:52 +0700

    Install wordpress

---
+++ previews/templates/menu.php
@@ -0,0 +1,134 @@
+<ul id="main-navigation">
+    <li class="active">
+        <a href="index.php">Home</a>
+        <ul class="sub-menu-dropdown">
+            <li><a href="homepage2.php">Homepage 2</a></li>
+            <li><a href="homepage3.php">Homepage 3</a></li>
+            <li><a href="homepage4.php">Homepage 4</a></li>
+            <li><a href="homepage5.php">Homepage 5</a></li>
+            <li><a href="homepage6.php">Homepage 6</a></li>
+        </ul>
+    </li>
+
+    <li><a href="pages-about.php">About us</a></li>
+    <li>
+        <a href="woocommerce-3columns-sidebar.php">Men</a>
+        <ul class="sub-menu-dropdown">
+            <li><a href="#">Accessories</a></li>
+            <li><a href="#">ASOS Basics</a></li>
+            <li><a href="#">Bags</a></li>
+            <li>
+                <a href="#">Caps & Hats</a>
+                <ul class="sub-menu-dropdown">
+                    <li><a href="#">Long Sleeve T-Shirts</a></li>
+                    <li><a href="#">Loungewear</a></li>
+                    <li><a href="#">Oversized & Longline</a></li>
+                    <li><a href="#">Polo Shirts</a></li>
+                    <li><a href="#">Shirts</a></li>
+                    <li><a href="#">Shoes, Boots & Trainers</a></li>
+                    <li><a href="#">Shorts</a></li>
+                    <li><a href="#">Suits & Blazers</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Gifts</a></li>
+            <li><a href="#">Grooming</a></li>
+            <li><a href="#">Hoodies & Sweatshirts</a></li>
+            <li><a href="#">Jackets & Coats</a></li>
+        </ul>
+
+    </li>
+    <li class="kt-megamenu-item megamenu-width-100">
+        <a href="#">Women</a>
+
+
+        <div class="kt-megamenu-wrapper">
+                <div class="kt-megamenu-inner megamenu-columns-4 megamenu-position-center">
+                    <ul class="kt-megamenu-ul">
+                        <li>
+                            <span>Shop Style</span>
+                            <ul class="sub-menu-megamenu">
+                                <li><a href="woocommerce-3columns.php">3 Column</a></li>
+                                <li><a href="woocommerce-3columns-sidebar.php">3 Column with sidebar</a></li>
+                                <li><a href="woocommerce-3columns.php">3 Column</a></li>
+                                <li><a href="woocommerce-4columns.php">4 Column</a></li>
+                                <li><a href="woocommerce-masonry.php">Masonry Style</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <span>Product Detail</span>
+                            <ul class="sub-menu-megamenu">
+                                <li><a href="woocommerce-product-detailed1.php">Product detail Style 1</a></li>
+                                <li><a href="woocommerce-product-detailed2.php">Product detail Style 2</a></li>
+                                <li><a href="woocommerce-product-detailed3.php">Product detail Style 3</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <span>Shop page</span>
+                            <ul class="sub-menu-megamenu">
+                                <li><a href="woocommerce-checkout.php">Checkout</a></li>
+                                <li><a href="woocommerce-compare.php">Compare</a></li>
+                                <li><a href="woocommerce-myaccount.php">My account</a></li>
+                                <li><a href="woocommerce-cart.php">My cart</a></li>
+                                <li><a href="woocommerce-wishlist.php">Wishlist</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <span>POPULAR BRANDS</span>
+                            <ul class="sub-menu-megamenu">
+                                <li><a href="woocommerce-checkout.php">Checkout</a></li>
+                                <li><a href="woocommerce-compare.php">Compare</a></li>
+                                <li><a href="woocommerce-myaccount.php">My account</a></li>
+                                <li><a href="woocommerce-cart.php">My cart</a></li>
+                                <li><a href="woocommerce-wishlist.php">Wishlist</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <div class="kt-megamenu-images">
+                        <div class="row">
+                            <div class="col-md-6"><a href="#"> <img src="assets/images/adv.jpg" alt=""/> </a></div>
+                            <div class="col-md-6"><a href="#"> <img src="assets/images/adv.jpg" alt=""/> </a></div>
+                        </div>
+                    </div>
+
+                </div>
+        </div>
+
+
+    </li>
+    <li><a href="#">accessories</a>
+        <ul class="sub-menu-dropdown">
+            <li><a href="shortcode-tabs.php">Tabs</a></li>
+            <li><a href="shortcode-banner.php">Banner</a></li>
+            <li><a href="shortcode-messagebox.php">Message Box</a></li>
+            <li><a href="shortcode-typography.php">Typography</a></li>
+            <li><a href="shortcode-iconbox.php">Icon box</a></li>
+            <li><a href="shortcode-blog-carousel.php">Blog carousel</a></li>
+            <li><a href="shortcode-carousel.php">Carousel</a></li>
+            <li><a href="shortcode-image.php">Image</a></li>
+            <li><a href="shortcode-heading.php">Heading</a></li>
+        </ul>
+    </li>
+    <li>
+        <a href="#">Blog</a>
+        <ul class="sub-menu-dropdown">
+            <li><a href="blog-list-sidebar.php">List</a></li>
+            <li><a href="blog-fullwidth.php">Full width</a></li>
+            <li>
+                <a href="blog-list-sidebar.php">Grid</a>
+                <ul class="sub-menu-dropdown">
+                    <li><a href="blog-grid-2columns-sidebar.php">2 column with sidebar</a></li>
+                    <li><a href="blog-grid-3columns.php">3 column</a></li>
+                </ul>
+            </li>
+            <li>
+                <a href="blog-masonry-3columns.php">Masonry</a>
+                <ul class="sub-menu-dropdown">
+                    <li><a href="blog-masonry-2columns-sidebar.php">2 column with sidebar</a></li>
+                    <li><a href="blog-masonry-3columns.php">3 column</a></li>
+                </ul>
+            </li>
+            <li><a href="blog-singlepost-standard.php">Blog detail</a></li>
+        </ul>
+    </li>
+    <li><a href="pages-contact.php">Contact</a></li>
+</ul><!-- #main-navigation -->
